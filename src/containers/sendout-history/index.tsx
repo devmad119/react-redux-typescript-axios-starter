21ad057f08122b17eaeafa5e70979bd7ac3d5f1c
GoldenFlash119 <devalpha18@gmail.com>
2021-12-07 07:27:52 +0900

    Fix: update the sendout-history page

--- src/containers/sendout-history/index.tsx
+++ src/containers/sendout-history/index.tsx
@@ -1,7 +1,110 @@
 import React from 'react';
+import { Table } from 'antd';
 
-const SenoutHistory:React.FC = () => {
-  return <>Sendout History</>;
+const columns: any[] = [
+  {
+    title: 'Name',
+    dataIndex: 'name',
+    filters: [
+      {
+        text: 'Joe',
+        value: 'Joe',
+      },
+      {
+        text: 'Category 1',
+        value: 'Category 1',
+        children: [
+          {
+            text: 'Yellow',
+            value: 'Yellow',
+          },
+          {
+            text: 'Pink',
+            value: 'Pink',
+          },
+        ],
+      },
+      {
+        text: 'Category 2',
+        value: 'Category 2',
+        children: [
+          {
+            text: 'Green',
+            value: 'Green',
+          },
+          {
+            text: 'Black',
+            value: 'Black',
+          },
+        ],
+      },
+    ],
+    filterMode: 'tree',
+    filterSearch: true,
+    onFilter: (value: any, record: any) => record.name.includes(value),
+    width: '30%',
+  },
+  {
+    title: 'Age',
+    dataIndex: 'age',
+    sorter: (a: any, b: any) => a.age - b.age,
+  },
+  {
+    title: 'Address',
+    dataIndex: 'address',
+    filters: [
+      {
+        text: 'London',
+        value: 'London',
+      },
+      {
+        text: 'New York',
+        value: 'New York',
+      },
+    ],
+    onFilter: (value: any, record: any) => record.address.startsWith(value),
+    filterSearch: true,
+    width: '40%',
+  },
+];
+
+const data = [
+  {
+    key: '1',
+    name: 'John Brown',
+    age: 32,
+    address: 'New York No. 1 Lake Park',
+  },
+  {
+    key: '2',
+    name: 'Jim Green',
+    age: 42,
+    address: 'London No. 1 Lake Park',
+  },
+  {
+    key: '3',
+    name: 'Joe Black',
+    age: 32,
+    address: 'Sidney No. 1 Lake Park',
+  },
+  {
+    key: '4',
+    name: 'Jim Red',
+    age: 32,
+    address: 'London No. 2 Lake Park',
+  },
+];
+
+const onChange = (pagination: any, filters: any, sorter: any, extra: any) => {
+  console.log('params', pagination, filters, sorter, extra);
+};
+
+const SenoutHistory: React.FC = () => {
+  return (
+    <>
+      <Table columns={columns} dataSource={data} onChange={onChange} />
+    </>
+  );
 };
 
 export default SenoutHistory;
