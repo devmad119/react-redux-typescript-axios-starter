db3268e5b83a818c049e5352f2229e6178c9cc86
kiddodev050 <kiddodev050@gmail.com>
2022-07-15 20:19:19 +0100

    Fix

--- src/containers/add-funds/index.tsx
+++ src/containers/add-funds/index.tsx
@@ -15,43 +15,43 @@ const AddFunds: React.FC = () => {
       <CardSection>
         <HighlightCard colors={['rgb(251,89,29)', 'rgb(247,180,51)']}>
           <TopSection>
-            <BsCurrencyBitcoin/>
+            <BsCurrencyBitcoin />
             <span>Bitcoin</span>
           </TopSection>
           <CenterSection>
-            <BsCurrencyBitcoin/>
+            <BsCurrencyBitcoin />
             <span>Any amount is acceptable</span>
           </CenterSection>
           <BottomSection>
-            <AiOutlinePlus/>
+            <AiOutlinePlus />
             <span>Add Balance Using Bitcoin Currency</span>
           </BottomSection>
         </HighlightCard>
         <HighlightCard colors={['rgb(82,100,117)', 'rgb(45,63,81)']}>
           <TopSection>
-            <FaEthereum/>
+            <FaEthereum />
             <span>Ether</span>
           </TopSection>
           <CenterSection>
-            <FaEthereum/>
+            <FaEthereum />
             <span>Any amount is acceptable</span>
           </CenterSection>
           <BottomSection>
-            <AiOutlinePlus/>
+            <AiOutlinePlus />
             <span>Add Balance Using Ethereum Currency</span>
           </BottomSection>
         </HighlightCard>
         <HighlightCard colors={['rgb(91,136,229)', 'rgb(75,165,225)']}>
           <TopSection>
-            <SiLitecoin/>
+            <SiLitecoin />
             <span>Lite Coin</span>
           </TopSection>
           <CenterSection>
-            <SiLitecoin/>
+            <SiLitecoin />
             <span>Any amount is acceptable</span>
           </CenterSection>
           <BottomSection>
-            <AiOutlinePlus/>
+            <AiOutlinePlus />
             <span>Add Balance Using Lite Coin Currency</span>
           </BottomSection>
         </HighlightCard>
@@ -59,43 +59,43 @@ const AddFunds: React.FC = () => {
       <CardSection>
         <HighlightCard colors={['rgb(90,135,229)', 'rgb(76,166,225)']}>
           <TopSection>
-            <FaBitcoin/>
+            <FaBitcoin />
             <span>Bitcoin Cash</span>
           </TopSection>
           <CenterSection>
-            <FaBitcoin/>
+            <FaBitcoin />
             <span>Any amount is acceptable</span>
           </CenterSection>
           <BottomSection>
-            <AiOutlinePlus/>
+            <AiOutlinePlus />
             <span>Add Balance Using Bitcoin Cash Currency</span>
           </BottomSection>
         </HighlightCard>
         <HighlightCard colors={['rgb(204, 82, 0)', 'rgb(255, 148, 77)']}>
           <TopSection>
-            <img src={XMRIcon} alt="xmr"/>
+            <img src={XMRIcon} alt="xmr" />
             <span>XMR</span>
           </TopSection>
           <CenterSection>
-            <img src={XMRIcon} alt="xmr" width={20} height={20}/>
+            <img src={XMRIcon} alt="xmr" width={20} height={20} />
             <span>Any amount is acceptable</span>
           </CenterSection>
           <BottomSection>
-            <AiOutlinePlus/>
+            <AiOutlinePlus />
             <span>Add Balance Using XMR Currency</span>
           </BottomSection>
         </HighlightCard>
         <HighlightCard colors={['rgb(51, 214, 255)', 'rgb(0, 163, 204)']}>
           <TopSection>
-            <img src={XRPIcon} alt="xmr" width={30} height={30}/>
+            <img src={XRPIcon} alt="xmr" width={30} height={30} />
             <span>XRP</span>
           </TopSection>
           <CenterSection>
-            <img src={XRPIcon} alt="xrp" width={20} height={20}/>
+            <img src={XRPIcon} alt="xrp" width={20} height={20} />
             <span>Any amount is acceptable</span>
           </CenterSection>
           <BottomSection>
-            <AiOutlinePlus/>
+            <AiOutlinePlus />
             <span>Add Balance Using XRP Currency</span>
           </BottomSection>
         </HighlightCard>
@@ -103,43 +103,43 @@ const AddFunds: React.FC = () => {
       <CardSection>
         <HighlightCard colors={['rgb(179, 0, 134)', 'rgb(210, 77, 255)']}>
           <TopSection>
-            <img src={TetherIcon} alt="xmr" width={30} height={30}/>
+            <img src={TetherIcon} alt="xmr" width={30} height={30} />
             <span>Tether</span>
           </TopSection>
           <CenterSection>
-            <img src={TetherIcon} alt="xrp" width={20} height={20}/>
+            <img src={TetherIcon} alt="xrp" width={20} height={20} />
             <span>Any amount is acceptable</span>
           </CenterSection>
           <BottomSection>
-            <AiOutlinePlus/>
+            <AiOutlinePlus />
             <span>Add Balance Using Tether Currency</span>
           </BottomSection>
         </HighlightCard>
         <HighlightCard colors={['rgb(22,81,95)', 'rgb(108,173,126)']}>
           <TopSection>
-            <FaHandHoldingUsd/>
+            <FaHandHoldingUsd />
             <span>USD Tron/TRC-20</span>
           </TopSection>
           <CenterSection>
-            <FaHandHoldingUsd/>
+            <FaHandHoldingUsd />
             <span>Any amount is acceptable</span>
           </CenterSection>
           <BottomSection>
-            <AiOutlinePlus/>
+            <AiOutlinePlus />
             <span>Add Balance Using USD Tron/TRC-20</span>
           </BottomSection>
         </HighlightCard>
         <HighlightCard colors={['rgb(149,1,32)', 'rgb(69,9,20)']}>
           <TopSection>
-            <GiMoneyStack/>
+            <GiMoneyStack />
             <span>Perfect Money</span>
           </TopSection>
           <CenterSection>
-            <GiMoneyStack/>
+            <GiMoneyStack />
             <span>Min/Max is 20 USD/5000 USD</span>
           </CenterSection>
           <BottomSection>
-            <AiOutlinePlus/>
+            <AiOutlinePlus />
             <span>Add Balance Using Perfect Money</span>
           </BottomSection>
         </HighlightCard>
